bae17f64c44fb0c42a3f182caff4d3dbf115e7c5
lesky <benjamin.lesky@gmx.de>
2013-12-19 11:26:36 +0100

    Main C aufgeräumt:
    Bis auf Initialiesierungen alles gelöscht
    
    Signed-off-by: lesky <benjamin.lesky@gmx.de>

--- Seegangskompensation/Seegangskompensation/main.c
+++ Seegangskompensation/Seegangskompensation/main.c
@@ -5,7 +5,7 @@
 * 														*
 * I/O-Konfiguration:									*
 * LCD 			-> Port 2								*
-* SDA			-> 1.0
+* SDA			-> 1.0									*
 * SCL 			-> 1.1			 						*
 * IN1		 	-> 1.3									*
 * IN2			-> 1.5									*
@@ -26,15 +26,6 @@
 #include "PSoCAPI.h"    
 #include <stdlib.h>
 
-// Präprozessor:Für testzwecke 
-// #defein Test nicht auskomentieren:
-#define TEST
-
-// Funktionsprototypen:
-void LCDansteuern(char);
-void Dateneinlesen(void);
-void Ausgangansteuern(char, char);
-void Initalisierung(void);
 // globale Structur zur Übergabe der Prozessdaten:
 struct 
    {	  						
@@ -42,161 +33,19 @@ struct
 	char pdchSollwert;								
 	} prozess;
 
+// Funktionsprototypen:
+void Initalisierung(void);
 
 void main(void)
 	{
 	
-	/********************************************************
-	* 			Deffinitionen und Deklarationen				*
-	********************************************************/	
-		
-	// Konstanten					
-	char kochKR;
-	char kochKRZ;
-		
-	// Variablen
-	char hichAusgangswert;							
-	char hichBeschleunigungssumme;  
-		
-	// Initialisierung des Controllers
+	
 	Initalisierung();
 	
-	// Präprozessor: kompiliere whileschleife wenn kein test
-	#ifndef TEST
 		
-		/********************************************************
-		* 			Endlosschleife für Dauerbetrieb				*
-		********************************************************/
-
-		while(1) 
-			{
-			// Daten Einlesen
-			
-			Dateneinlesen();		                         
-			// Parameter Berechnen
-			
-			hichBeschleunigungssumme = hichBeschleunigungssumme + prozess.pdchBechleunigung;
-			
-			hichAusgangswert = ( prozess.pdchSollwert - prozess.pdchBechleunigung ) * kochKR
-								- kochKRZ * hichBeschleunigungssumme;
-			
-			// Daten Ausgeben:
-				
-			Ausgangansteuern(hichAusgangswert);
-			
-			LCDansteuern(prozess.pdchEntfernung);
-
-				
-			};
-			
-	/********************************************************
-	* Achtung die Folgenden Zeilen dienen nur zu Testzwecken*
-	* und sind für das Fertige Programm nicht relevant.		*
-	* Sie werden anstelle der Regulären while Schleife 		*
-	* Kompiliert, wenn die Bedingung #ifndef TEST nicht 	*
-	* erfüllt ist.											*
-	********************************************************/
-
-	#else
-						
-		// whileschleife zu testzwecken
-		// Konstante zur verweildauer in der schleife
-		while (1)
-			{
-				Dateneinlesen();
-				//Ausgangansteuern(250, 2);
-				// LCDansteuern(1);
-				//if (PRT0DR & 0x01)
-				//{LCDansteuern(1);}
-				//else {LCDansteuern(PRT0DR & 0x01);}
-					//LCDansteuern(PRT0DR & 0x01);
-				/*
-				// Erkennen von eingangssignalen über A/D-Wandler
-				if (prozess.pdchEntfernung <= 110)
-				{
-					LCDansteuern(1);
-					Ausgangansteuern(250,1);}
-				else if (prozess.pdchBechleunigung <= 110)
-				{	LCDansteuern(2);
-					Ausgangansteuern(250,2);}
-				else {
-				Ausgangansteuern(0,0);	
-				}
-				*/
-				LCDansteuern(prozess.pdchSollwert);
-			};
-			
-	// Präprozessor: Ende der Verzweifung
-	#endif 
 }
 	
-/********************************************************
-* 			Funktionsdeklarationen						*
-********************************************************/
-	
-void LCDansteuern(char hichdata)
-	{		
-			char rgch[5];
-			// Text auf LCD ausgeben
-			LCD_1_Position(0,5);    
-			LCD_1_PrCString("Wert:");
-			
-			// Zahl Auf LCD ausgeben
-			itoa(rgch,hichdata,10);
-			LCD_1_PrString(rgch);		
 
-	}
-	
-void Dateneinlesen(void)
-	{	
-		// I2C Adresse: 1001 000 -> 0x48
-		// Einleseforgang beginnen
-		I2Cm_fSendRepeatStart(0x48,I2Cm_READ);
-		
-		// Daten Einlesen und ACK an slafe senden
-		prozess.pdchBechleunigung = I2Cm_bRead(I2Cm_ACKslave);
-		prozess.pdchEntfernung	= I2Cm_bRead(I2Cm_ACKslave);
-		
-		// lezte Daten ohne ACK einlesen
-		prozess.pdchSollwert = I2Cm_bRead(I2Cm_NAKslave);
-		
-		//Stop Condition
-       	I2Cm_SendStop();
-
-	}
-
-	void Ausgangansteuern(char hichAusgangswert, char hichRichtung)
-	{
-		// Drehrichtung Auswählen
-		switch(hichRichtung) {
-			
-			// Linkslauf
-			case 1: 
-				// IN1 und IN2 Ansteuern
-				IN1_Switch(0);
-				IN2_Switch(1);
-				break;
-			
-			// Rechtslauf
-			case 2:
-				
-				// IN1 und IN2 Ansteuern
-				IN1_Switch(1);
-				IN2_Switch(0);
-				break;
-				
-			// Ungültiger Wert
-			default:
-				// Pulsweite auf 0 setzen
-				PWM8_1_WritePulseWidth(0);
-				// Funktion verlassen
-				return ;
-				
-			}
-		
-		// Pulsweite auf hichAusgangswert setzen 		
-		PWM8_1_WritePulseWidth(hichAusgangswert);
-	}
 
 void Initalisierung(void)
 	{
@@ -205,10 +54,10 @@ void Initalisierung(void)
 		
 	// I2C Starten
 	I2Cm_Start();
-  	// A/D Wandler konfigurieren
-	I2Cm_fSendStart(0x48,I2Cm_WRITE);       
-    I2Cm_fWrite(0x00);
-	I2Cm_SendStop();
+//  	// A/D Wandler konfigurieren
+//	I2Cm_fSendStart(0x48,I2Cm_WRITE);       
+//    I2Cm_fWrite(0x00);
+//	I2Cm_SendStop();
 	// Initialisieren des LCD-Displays
 	LCD_1_Start();                 					
    	
